680e452768ffce0589d7f53a94588173277597fd
Jhonni1000 <akintoyeopeyemi5@gmail.com>
2025-06-13 14:25:41 +0200

    test

--- src/terraform/s3_static.tf
+++ src/terraform/s3_static.tf
@@ -6,3 +6,43 @@ resource "aws_s3_bucket" "epic_reads_static_website" {
         managed_by = "OPAKI"
     }
 }
+
+resource "aws_s3_bucket_website_configuration" "epic_reads_static_website" {
+    bucket = aws_s3_bucket.epic_reads_static_website.id
+
+    index_document {
+        suffix = "index.html"
+    }
+
+    error_document {
+        key = "error.html"
+    }
+}
+
+resource "aws_s3_bucket_public_access_block" "epic_reads_static_website" {
+    bucket = aws_s3_bucket.epic_reads_static_website.id
+
+    block_public_acls =  false
+    block_public_policy = false
+    ignore_public_acls = false
+    restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "epic_reads_static_website" {
+    bucket = aws_s3_bucket.epic_reads_static_website.id
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Action = "s3:GetObject"
+                Effect = "Allow"
+                Principal = "*"
+                Resource = "${aws_s3_bucket.epic_reads_static_website.arn}/*"
+            }
+        ]
+    })
+}
+
+output "website_url" {
+    value = aws_s3_bucket_website_configuration.epic_reads_static_website.website_endpoint
+}
