534c34d6d513f767467de0db0a10fbd32eb3b1af
Jhonni1000 <akintoyeopeyemi5@gmail.com>
2025-06-16 22:15:45 +0200

    fix folder organisation

--- src/terraform/contact_lambda.tf
+++ src/terraform/contact_lambda.tf
@@ -17,18 +17,6 @@ resource "aws_iam_role" "contact_lambda_role" {
   assume_role_policy = data.aws_iam_policy_document.contact_lambda_policy.json
 }
 
-resource "aws_iam_role_policy" "contact_lambda_policy" {
-  name = "contact_lambda_policy"
-  role = aws_iam_role.contact_lambda_role.name
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect   = "Allow"
-      Action   = "ses:SendEmail"
-      Resource = ["*"]
-    }]
-  })
-}
 
 module "lambda_function" {
   source = "terraform-aws-modules/lambda/aws"
